f54997b29c4213c340b2b344ee2f3cf8920ab5ca
Gerges-Creative <gerges.zechariah@gmail.com>
2024-02-18 12:06:06 -0800

    Bubble Sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -19,7 +19,7 @@ void bubble_sort(int *array, size_t size)
 	{
 		checker = 0;
 
-		for ( j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
