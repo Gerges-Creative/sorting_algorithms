b606ee94176e1aee7152e92faa61cd12f4ac00c5
Gerges-Creative <gerges.zechariah@gmail.com>
2024-02-19 13:05:35 -0800

    Insertion Sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -56,7 +56,7 @@ void insertion_sort_list(listint_t **list)
 
 	head = *list;
 
-	if (head == NULL || head->next == NULL)
+	if (!list || head == NULL || head->next == NULL)
 		return;
 
 	*list = (*list)->next;
