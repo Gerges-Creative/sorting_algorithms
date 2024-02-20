ddb203c053b3473898c0096dc45ee6c3b19829fb
Gerges-Creative <gerges.zechariah@gmail.com>
2024-02-19 16:06:02 -0800

    Selection Sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,24 +9,25 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int hold, min;
-	if (size < 2)
+	size_t i, j, min;
+	int hold;
+
+	if (size < 2 || array == NULL)
 		return;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		hold = array[i];
-		min = i + 1;
+		min = i;
 
-		for (j = i + 1; j < size - 1; j++)
+		for (j = i + 1; j < size; j++)
 		{
-			if (array[min] > array[j + 1])
-				min = j + 1;
+			if (array[j] < array[min])
+				min = j;
 		}
 
-		if (array[min] < hold)
+		if (min != i)
 		{
+			hold = array[i];
 			array[i] = array[min];
 			array[min] = hold;
 			print_array(array, size);
