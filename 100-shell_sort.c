8a662fd7388fde1afdd59746952c6fe7b1252b75
Gerges-Creative <gerges.zechariah@gmail.com>
2024-02-24 13:49:23 -0800

    Shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -12,25 +12,24 @@ void shell_sort(int *array, size_t size)
 	size_t i, j, gap;
 	int temp;
 
-	while (gap <= size  / 3)
+	if (!array || !size)
+		return;
+
+	while (gap < size)
 		gap = gap * 3 + 1;
 
-	while (gap > 0)
+	for (gap = (gap - 1) / 3; gap; gap = (gap - 1) / 3)
 	{
 		for (i = gap; i < size; i++)
 		{
 			temp = array[i];
 
-			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+			for (j = i; j > gap - 1 && array[j - gap] > temp; j -= gap)
 			{
 				array[j] = array[j - gap];
 			}
-
 			array[j] = temp;
 		}
-
-		gap = (gap - 1) / 3;
-
 		print_array(array, size);
 	}
 }
